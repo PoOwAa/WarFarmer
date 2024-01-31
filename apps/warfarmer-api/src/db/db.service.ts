f9beac152afe4c9d161ba95a6f910992b15cfca6
Raymund Ács <raycsucsu@gmail.com>
2024-01-31 21:10:19 +0100

    Refactor prisma schema

--- apps/warfarmer-api/src/db/db.service.ts
+++ apps/warfarmer-api/src/db/db.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client/warfarmer';
 
 @Injectable()
 export class DbService implements OnModuleInit {
